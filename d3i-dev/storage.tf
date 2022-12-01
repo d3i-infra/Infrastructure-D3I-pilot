dcf8d03b32bde9bfec45f66293ec135104ca8624
trbKnl <ncdeschipper@gmail.com>
2022-12-01 15:43:05 +0100

    added SAS key

--- d3i-dev/storage.tf
+++ d3i-dev/storage.tf
@@ -5,6 +5,7 @@ resource "azurerm_storage_account" "sa" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
   queue_properties {
     logging {
       read                  = true
@@ -33,4 +34,36 @@ resource "azurerm_key_vault_secret" "sakey" {
   name         = "sakey"
   value        = azurerm_storage_account.sa.primary_access_key
   key_vault_id = azurerm_key_vault.kv.id
+}
+
+
+data "azurerm_storage_account_blob_container_sas" "sastoken" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  container_name    = azurerm_storage_container.sc.name
+  https_only        = true
+
+  #ip_address = "168.1.5.65"
+
+  start  = "2022-11-29"
+  expiry = "2023-11-29"
+
+  permissions {
+    read   = false
+    add    = false
+    create = true
+    write  = false
+    delete = false
+    list   = false
+  }
+
+  cache_control       = "max-age=5"
+  content_disposition = "inline"
+  content_encoding    = "deflate"
+  content_language    = "en-US"
+  content_type        = "application/json"
+}
+
+output "sas_url_query_string" {
+  value = data.azurerm_storage_account_blob_container_sas.sastoken.sas
+  sensitive = true
 }
